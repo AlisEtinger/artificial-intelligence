34c0e536d2626edd8c2b738dbd1868435446a107
Alisa <112906693+AlisEtinger@users.noreply.github.com>
2022-09-15 20:21:40 +0300

    Add files via upload

---
+++ Лотерея исход событий .py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+if __name__ == '__main__':
+    num=0
+    for c1 in range(1,25):
+        for c2 in range(c1+1,26):
+            for c3 in range(c2+1,27):
+                for c4 in range(c3+1,28):
+                    for c5 in range(c4+1,29):
+                        for c6 in range(c5+1,30):
+                            for c7 in range(c6+1,31):
+                                print(c1,c2,c3,c4,c5,c6,c7)
+
+
+# In[ ]:
+
+
+
+
